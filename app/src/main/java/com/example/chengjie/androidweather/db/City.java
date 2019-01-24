c6a395f6ddb8fa462cb4ad5b7e6b5265a744853b
cj815815 <chengjie@git.com>
2019-01-24 16:35:45 +0800

    Second commit

---
+++ app/src/main/java/com/example/chengjie/androidweather/db/City.java
@@ -0,0 +1,49 @@
+package com.example.chengjie.androidweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by machenike on 2019/1/24.
+ */
+
+public class City extends DataSupport {
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;  //记录当前市所属的省的id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/chengjie/androidweather/db/Province.java
@@ -0,0 +1,39 @@
+package com.example.chengjie.androidweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by machenike on 2019/1/24.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
